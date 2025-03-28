7f1d8b0da0084e93e380d5051a548d818ec85295
FadyASulaiman <sulaiman.a.fady@gmail.com>
2025-03-28 09:57:33 +0200

    Add FinBERT tokenizer

--- src/preprocessors/data_preprocessors/finbert_tokenizer.py
+++ src/preprocessors/data_preprocessors/finbert_tokenizer.py
@@ -6,11 +6,11 @@ from utils.constants import SPECIAL_TOKENS
 
 class FinBERTTokenizer(PreprocessorBase):
     """Class to tokenize text using FinBERT tokenizer"""
-    
+
     def __init__(self, finbert_model="ProsusAI/finbert"):
         self.finbert_model = finbert_model
         self.tokenizer = None
-    
+
     def fit(self, X, y=None):
         """Load the tokenizer"""
         logger.info(f"Loading FinBERT tokenizer from {self.finbert_model}")
@@ -22,22 +22,22 @@ class FinBERTTokenizer(PreprocessorBase):
             logger.info("Falling back to standard BERT tokenizer")
             self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
         return self
-    
+
     def transform(self, X):
         """Tokenize text using FinBERT tokenizer"""
         logger.info("Tokenizing text using FinBERT tokenizer")
-        
+
         if self.tokenizer is None:
             self.fit(X)
-        
+
         # Tokenize each text
         tokenized = []
         for text in X:
             # Add special tokens at the start and end
             text = f"{SPECIAL_TOKENS['START']} {text} {SPECIAL_TOKENS['END']}"
-            
+
             # Tokenize
             tokens = self.tokenizer.tokenize(text)
             tokenized.append(tokens)
-        
+
         return pd.Series(tokenized)
